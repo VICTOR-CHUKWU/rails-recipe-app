521036863c53acf3839e887df73489132baf2696
victor-chukwu <okenwa1993@gmail.com>
2022-02-08 19:57:13 +0100

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,14 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+  tufoin = User.create!( name: "Tufoin" ,role: "user")
+  victor = User.create!( name: "victor" ,role: "user")
+  tolu = User.create!( name: "Tolu" ,role: "admin")
+
+  rice = victor.foods.create!( name: "Rice" , measurement_unit: "kg", price: 2000 )
+  plantain = tufoin.foods.create!( name: "plantain" , measurement_unit: "kg", price: 1000 )
+  chicken = tufoin.foods.create!( name: "chicken breast" , measurement_unit: "kg", price: 3000 )
+  pizza = tolu.foods.create!( name: "pizza" , measurement_unit: "kg", price: 6000 )
+  soup = tolu.foods.create!( name: "pepper soup" , measurement_unit: "kg", price: 3000 )
 #   Character.create(name: "Luke", movie: movies.first)
+
