843dc66e7362120e86a47e4ab513eb7782b86f16
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-02-12 11:23:03 +0100

    Add recipe and user test

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -8,45 +8,29 @@ describe Food, type: :model do
 
   it 'Food should be valid' do
     expect(@soya_beans).to be_valid
-  end
+    end
 
-  it 'Food should not be valid without a name' do
-    @soya_beans.name = nil
-    expect(@soya_beans).to_not be_valid
-    @soya_beans.name = ''
-    expect(@soya_beans).to_not be_valid
-  end
+  it 'validates the presence of the measurement unit nil' do
+      @food.measurement_unit = nil
+      expect(@food).to_not be_valid
+    end
 
-  it 'Food should not be valid if name is too long' do
-    @soya_beans.name = 'g' * 251
-    expect(@soya_beans).to_not be_valid
-  end
+  it 'validates the presence of the price nil' do
+      @food.price = nil
+      expect(@food).to_not be_valid
+    end
 
-  it 'Food should not be valid without a measurement unit' do
-    @soya_beans.measurement_unit = nil
-    expect(@soya_beans).to_not be_valid
-    @user.name = ''
-    expect(@user).to_not be_valid
-  end
+  it 'validates the presence of the user nil' do
+      @food.user_id = nil
+      expect(@food).to_not be_valid
+    end
 
-  it 'Food should not be valid if wrong measurement unit is passed' do
-    @soya_beans.measurement_unit = 15
-    expect(@soya_beans).to_not be_valid
-    @soya_beans.name = true
-    expect(@soya_beans).to_not be_valid
-    @soya_beans.name = 'gl'
-    expect(@soya_beans).to_not be_valid
-  end
+    it 'validates the presence of the user nil' do
+      @food.user_id = nil
+      expect(@food).to_not be_valid
+      end
 
-  it 'Food should not be valid if price is negative' do
-    @soya_beans.price = - 15
-    expect(@soya_beans).to_not be_valid
-  end
-
-  it 'Food should not be valid if price is not a number' do
-    @soya_beans.price = 'fifteen'
-    expect(@soya_beans).to_not be_valid
-    @soya_beans.price = true
-    expect(@soya_beans).to_not be_valid
-  end
+  it 'validates the food name match' do
+      expect(@food.name).to eq "Food 1"
+    end
 end
